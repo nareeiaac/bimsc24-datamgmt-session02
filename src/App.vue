7814ba32f5d5fbed16dff5bded08c3600a05fe12
nareeiaac <155987085+nareeiaac@users.noreply.github.com>
2024-01-28 02:20:57 +0800

    Assignment 02 Update 240126 NP

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,27 @@
 <script setup>
 import { ref } from "vue" // you need this to use ref()
 
+//Ner reference variable
+let user = ref("- Naree")
 
 </script>
 
 
 
+
 <!-- the template is where the html code goes -->
 <template>
+ <div id="navbar" class="container">  
+        <div id="title">Data Management Assignment 02 {{ user }}</div>
+       
+    </div>
+
+    <div id="flex">
+
+        <div id="sidebar" class="container"> Input</div>
+
+        <div id="main" class="container"> Output </div>
+    </div>
 
 
 
@@ -19,5 +33,62 @@ import { ref } from "vue" // you need this to use ref()
 <!-- style is where the css code goes -->
 <style scoped>
 
+html{
+    
+    background-color: rgb(59, 59, 59); 
+    color: white;
+}
 
+body{
+
+    margin:0;
+}
+
+div{
+
+    /* put the borders in the inside of container */
+    box-sizing: border-box;
+}
+
+#navbar{
+
+    height: 50px;
+    border-color: grey;
+background-color: pink;
+
+}
+
+#flex{
+    
+    display: flex;
+    height: calc(100vh - 50px);
+}
+
+#sidebar{
+    
+    width:20%;
+    border-color: grey;
+background-color: white;
+color: black;
+}
+
+#main{
+    width:80%;
+    border-color: grey;
+background-color: lemonchiffon;
+
+
+}
+
+#title{
+    width: 70%;
+    float:left;
+}
+
+.container{
+
+    border-style: dotted;
+    border-width: 1px;
+
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -13,3 +13,66 @@ import '@/styles/toggle-styles.css'
 
 // Create Vue appliction with App.vue component and inject it into element with id 'app'
 createApp(App).mount("#app")
+
+//link the js script file with the website created by using a constant
+
+const doc = document.documentElement;
+console.log(doc)
+
+//access particular elements
+const sidebar = document.querySelector('#sidebar')
+
+//create a form element in the sidebar
+let input = document.createElement("input")
+input.style.width = "90%"
+input.style.fontsize = "10px"
+sidebar.appendChild(input)
+
+
+//make the submit button
+let submitbutton = document.createElement("button")
+sidebar.appendChild(submitbutton)
+//what should be written on the button
+submitbutton.innerHTML="Submit"
+submitbutton.style.fontSize="20px"
+
+
+//add rest button, first selected where to add so call it
+let resetbutton = document.createElement("button")
+sidebar.appendChild(resetbutton)
+//what should be written on the button
+resetbutton.innerHTML="Reset"
+submitbutton.style.fontSize="20px"
+
+//create an event when you click the button, attatch a function to it
+
+submitbutton.addEventListener("click",dosomething)
+resetbutton.addEventListener('click',Refresh)
+
+//selecting where you want the change
+const main = document.querySelector("#main")
+
+function dosomething(){
+//create a p element
+let p = document.createElement("p")
+
+//fill that p element with some text
+let inputext = input.value
+    p.innerHTML += inputext
+    console.log(inputext)
+    
+// append that element to main
+main.appendChild(p)
+
+
+ //just = will replace text, += will keep adding to it
+
+//console.log(inputText) // to check on the console of this is working, which means whatever you type in the box you get it in the console
+
+
+}
+
+function Refresh(){
+    let p = document.querySelectorAll("p")
+    main.innerHTML = ""
+}
